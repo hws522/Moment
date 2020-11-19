91035ef1670b5f81d7c003509f13599aa244b748
hws522 <sraphic@kakao.com>
2020-11-19 23:23:54 +0900

    css add

--- clock.js
+++ clock.js
@@ -8,6 +8,8 @@ function getTime(){
     const hours = date.getHours();
     const seconds = date.getSeconds();
     clockTitle.innerText = `${hours < 10 ? `0${hours}` : `${hours}`}:${minutes < 10 ? `0${minutes}` : `${minutes}`}:${seconds < 10 ? `0${seconds}` : `${seconds}`}`;
+
+    
 }
 
 function init(){
